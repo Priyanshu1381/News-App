bc68a730e0d1e3f933898b26c3773280b2e76ef5
Priyanshu1381 <Priyanshuverma138@gmail.com>
2023-04-25 00:03:57 +0530

    Final Commit

---
+++ app/src/main/java/com/example/newsbreeze/OnFetchDataListener.java
@@ -0,0 +1,10 @@
+package com.example.newsbreeze;
+
+import com.example.newsbreeze.Models.NewsHeadlines;
+
+import java.util.List;
+
+public interface OnFetchDataListener<NewsApiResponse> {
+    void onFetchData(List<NewsHeadlines> list, String message);
+    void onError(String message);
+}
---
+++ app/src/main/java/com/example/newsbreeze/SelectListener.java
@@ -0,0 +1,7 @@
+package com.example.newsbreeze;
+
+import com.example.newsbreeze.Models.NewsHeadlines;
+
+public interface SelectListener {
+    void OnNewsClicked(NewsHeadlines headlines);
+}
